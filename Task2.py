b48aff2806cc395e7ae719cedf51bb2f98ed2ba2
SeraviaSamuel <116890349+SeraviaSamuel@users.noreply.github.com>
2022-10-28 17:56:26 +0200

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[43]:
+
+
+from pyopenms import *
+seq = AASequence.fromString("VAKA")
+print("Total is =",seq.getMonoWeight())
+sum=0
+for aa in seq:
+        sum +=aa.getMonoWeight()
+print("Sum is =",sum)
+
+
+# In[ ]:
+
+
+
+
